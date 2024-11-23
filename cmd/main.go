d0fe65aa573722e6abe1b7b5d0d16eefed95776d
oriastanjung <oriastan999@gmail.com>
2024-11-23 13:14:22 +0700

    feat:finance service

--- cmd/main.go
+++ cmd/main.go
@@ -6,11 +6,16 @@ import (
 
 	"github.com/oriastanjung/grpc_server/internal/config"
 	"github.com/oriastanjung/grpc_server/internal/database"
-	auth_server "github.com/oriastanjung/grpc_server/internal/grpc/auth"
-	repository "github.com/oriastanjung/grpc_server/internal/repository/auth"
-	services "github.com/oriastanjung/grpc_server/internal/services/auth"
-	usecase "github.com/oriastanjung/grpc_server/internal/usecase/auth"
+	serverAuth "github.com/oriastanjung/grpc_server/internal/grpc/auth"
+	serverFinance "github.com/oriastanjung/grpc_server/internal/grpc/finance"
+	repositoryAuth "github.com/oriastanjung/grpc_server/internal/repository/auth"
+	repositoryFinance "github.com/oriastanjung/grpc_server/internal/repository/finance"
+	servicesAuth "github.com/oriastanjung/grpc_server/internal/services/auth"
+	servicesFinance "github.com/oriastanjung/grpc_server/internal/services/finance"
+	usecaseAuth "github.com/oriastanjung/grpc_server/internal/usecase/auth"
+	usecaseFinance "github.com/oriastanjung/grpc_server/internal/usecase/finance"
 	pbAuth "github.com/oriastanjung/grpc_server/proto/auth"
+	pbFinance "github.com/oriastanjung/grpc_server/proto/finance"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/reflection"
 )
@@ -29,11 +34,19 @@ func main(){
 	database.GracefulShutdown()
 
 	// auth service
-	authRepository := repository.NewAuthRepository(database.DB)
-	authUseCase := usecase.NewAuthUseCase(authRepository)
-	authService := services.NewAuthService(authUseCase)
-	authServer := auth_server.NewAuthServer(authService,config.BcryptSalt)
+	authRepository := repositoryAuth.NewAuthRepository(database.DB)
+	authUseCase := usecaseAuth.NewAuthUseCase(authRepository)
+	authService := servicesAuth.NewAuthService(authUseCase)
+	authServer := serverAuth.NewAuthServer(authService,config.BcryptSalt)
 	// end auth service
+
+
+	// finance service
+	financeRepository := repositoryFinance.NewFinanceRepository(database.DB)
+	financeUseCase := usecaseFinance.NewFinanceUseCase(financeRepository)
+	financeService := servicesFinance.NewFinanceService(financeUseCase)
+	fincanceServer := serverFinance.NewFinanceServer(financeService)
+	// end finance service
 	listener,err := net.Listen("tcp",addr)
     if err != nil{
         log.Printf("Error on Listening %v\n",err)
@@ -47,6 +60,7 @@ func main(){
     serverInstance := grpc.NewServer()
 
     pbAuth.RegisterAuthServiceRoutesServer(serverInstance, authServer)
+	pbFinance.RegisterFinanceRoutesServiceServer(serverInstance, fincanceServer)
     reflection.Register(serverInstance)
     if err = serverInstance.Serve(listener); err != nil{
         log.Fatalf("Failed on Serve %v\n",err)
